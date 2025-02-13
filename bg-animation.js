2e5892a2fd23fc6889cb596d9f7ffe52ac181ea1
mmoth123 <102495764+mmoth123@users.noreply.github.com>
2025-02-13 14:18:13 +0700

    bg-animation.js

--- bg-animation.js
+++ bg-animation.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const bgAnimation = document.querySelector(".background-animation");
+    const bgContainer = document.querySelector(".container");
 
-    function createMovingStars() {
-        for (let i = 0; i < 50; i++) {
+    function createStars() {
+        for (let i = 0; i < 50; i++) { // จำนวนดาวที่ต้องการ
             let star = document.createElement("div");
             star.classList.add("star");
             star.style.top = `${Math.random() * 100}vh`;
             star.style.left = `${Math.random() * 100}vw`;
             star.style.animationDuration = `${Math.random() * 5 + 5}s`;
-            bgAnimation.appendChild(star);
+            bgContainer.appendChild(star);
         }
     }
 
-    createMovingStars();
+    createStars(); // เรียกฟังก์ชันเพื่อสร้างดาว
 });
